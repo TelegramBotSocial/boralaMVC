d5f7adb44a01a360fb0d1790ee12e2e0318dfdb4
Beto Noronha <beto_noronha@live.com>
2017-11-28 11:01:32 -0200

    organize folder

--- source/src/main/Main.java
+++ source/src/main/Main.java
@@ -5,13 +5,13 @@ public class Main {
 	private static Model model;
 	
 	public static void main(String[] args) {
-
+		
 		model = Model.getInstance();
 		initializeModel(model);
 		View view = new View(model);
 		model.registerObserver(view);
 		view.receiveUsersMessages();
-
+		
 	}
 	
 	public static void initializeModel(Model model){
